113053697c0940412c09f2c61bc6ea9d5f02da7a
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-29 18:22:25 -0300

    Update $PROFILE.ps1

--- $PROFILE.ps1
+++ $PROFILE.ps1
@@ -1,12 +1,18 @@
 #goto Scripts for general projects take path and use it here
 #function gotoMoneyWeb {cd -Path "C:\Users\ynoa_mota\Documents\dev\money-admin-api"}
 #function gotoApolo {cd -Path "C:\Users\ynoa_mota\Documents\dev\apolo"}
+
+function devFolder {cd -Path "C:\dev"}
+
+#git context scripts
 function add { git add .}
 function status {git status}
 function push {git push}
 function pull {git pull}
 function containers {docker ps -a}
 
+#Modules
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
 Import-Module oh-my-posh
+#Theme
 Set-Theme Robbyrussell
