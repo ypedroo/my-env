63e240b0752f2f2279771fab7236f319edd5925d
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-09-09 13:52:27 -0300

    Update $PROFILE.ps1

--- $PROFILE.ps1
+++ $PROFILE.ps1
@@ -1,20 +1,27 @@
 #goto Scripts for general projects take path and use it here
-#function gotoMoneyWeb {cd -Path "C:\Users\ynoa_mota\Documents\dev\money-admin-api"}
-#function gotoApolo {cd -Path "C:\Users\ynoa_mota\Documents\dev\apolo"}
-
 function devFolder {cd -Path "C:\dev"}
+function gotoDrop {cd -Path "C:\dev\drop-csharp"}
+function gotoPlural {cd -Path "C:\dev\pluraL\pluralsight-exercises"}
+function gotoCli {cd -Path "C:\Dev\remote-cli\"}
+function gotoApiContracts {cd -Path "C:\Dev\contracts-api"}
 
 #git context scripts
 function add { git add .}
-function git st {git status}
+function status {git status}
 function push {git push}
-function pull {pull --rebase origin}
-function git co {git checkout}
-function git new {git checkout -b}
+function pull {git pull}
+function diff {git diff}
+function co {git checkout}
+function new {git checkout -b}
 function containers {docker ps -a}
-
+function jp {jupyter notebook}
 #Modules
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Import-Module posh-git
 Import-Module oh-my-posh
 #Theme
-Set-Theme Robbyrussell
+Set-Theme Powerlevel10k-Classic 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
