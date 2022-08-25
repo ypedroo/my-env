2d6a57e0c8d972e2346bfd3acaafd606ab32a6ff
Ynoa Pedro <ynoa.pedro@outlook.com>
2022-08-25 08:22:21 -0300

    Update ps-profile.ps1

--- windows-dotfiles/ps-profile.ps1
+++ windows-dotfiles/ps-profile.ps1
@@ -2,7 +2,7 @@
 function devf {Set-Location -Path "C:\users\ynoap\dev"}
 #Aliases
 Set-Alias g git
-Set-Alias vim neovim
+Set-Alias vim nvim
 function add {git add .}
 function st {git status}
 function push {git push}
@@ -11,13 +11,19 @@ function diff {git diff}
 function co {git checkout}
 function new {git checkout -b}
 function clone {git clone}
-function gcommit {git commit -a}
 #docker
 function containers {docker ps -a}
 #Imports
-Import-Module oh-my-posh
-Import-Module posh-git
+# Import-Module oh-my-posh
+# Import-Modul1e posh-git
 #by being in onedrive it will alway be synced on windows
-Set-PoshPrompt -Theme C:\Users\ynoap\OneDrive\Docs\oh-my-posh-main\themes\star.omp.json
+# Set-PoshPrompt -Theme C:\Users\ynoap\OneDrive\Docs\oh-my-posh-main\themes\star.omp.json
 Import-Module Terminal-Icons
 Invoke-Expression (&starship init powershell)
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
